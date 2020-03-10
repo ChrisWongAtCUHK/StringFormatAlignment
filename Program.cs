365dbe3e30841da607a6dafa08d6b3dba8763eea
Chris Wong <wc38.wong@gmail.com>
2020-03-10 18:08:45 +0800

    Simple string format

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringFormatAlignment
 {
     class Program
     {
+        static string FixedWidth(object value, int width, bool leftAlign)
+        {
+            var s = value == null ? string.Empty : value.ToString();
+            return leftAlign ? s.PadRight(width) : s.PadLeft(width);
+        }
+
+        class Entry
+        {
+            public string Name;
+            public int Score;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var data = new List<Entry>
+            {
+                new Entry { Name = "Jeffrey", Score = 32767 },
+                new Entry { Name = "Fox", Score = 255},
+                new Entry { Name = "Darkthread", Score = 9999}
+            };
+            data.ForEach(o =>
+            {
+                Console.WriteLine(
+                    $"{FixedWidth(o.Name, 16, true)} {FixedWidth(o.Score.ToString("N0"), 6, false)}");
+            });
         }
     }
 }
